1da5f02d53e0541da165fd7096b39006ca5f66ca
20221tiimi0220 <mariellyalcantara06@gmail.com>
2024-11-26 14:44:25 -0300

    Iniciando as páginas de agendamento Terminar activity_agendamento

--- app/src/main/java/arthur/francisco/gabi/mary/maissus/activities/CadastroActivity.java
+++ app/src/main/java/arthur/francisco/gabi/mary/maissus/activities/CadastroActivity.java
@@ -17,7 +17,7 @@ public class CadastroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.CONSTRAINT), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
